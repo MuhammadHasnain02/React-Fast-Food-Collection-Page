b61075e31f501b3c808bf4520d5be20ec1cc63f7
MuhammadHasnain02 <mrhasnain9009@gmail.com>
2025-11-09 19:16:57 +0500

    C-P

--- reactjs-collection-page/src/App.jsx
+++ reactjs-collection-page/src/App.jsx
@@ -1,15 +1,16 @@
 import { useState } from "react";
 import CategoryFilter from "./components/CategoryFilter";
+import Rating from "./components/Rating";
 
 import Products from "./components/Products";
-import { getVisibleProducts } from "./data/product-filters";
+import { products } from "./data/product";
 
 function App() {
 
   let [selectedCateg , setSelectedCateg] = useState([])
+  const [selectedRating, setSelectedRating] = useState(null);
 
-  let filteredProd = getVisibleProducts(selectedCateg)
-
+  // Handle Category Change
   let handleCategChange = (category , isChecked) => {
     
     if (isChecked) {
@@ -23,19 +24,43 @@ function App() {
 
   }
 
+  // Handle Rating Filter
+  const handleRatingFilter = (rat) => {
+    
+    console.log("Filtering by rating:", rat);
+    if (selectedRating === rat) return setSelectedRating(null)
+    else return setSelectedRating(rat)
+    
+  }
+
+  let filteredProd = products.filter(item => {
+
+    if (selectedCateg.length > 0) return selectedCateg.includes(item.category)
+    else return true
+    
+  }).filter(item => {
+
+    if (selectedRating) return item.rating >= selectedRating
+    else return true
+
+  })
+
   return (
-    <div>
-      <div className="grid grid-cols-12 my-3">
-        <div className="col-span-2">
-          <CategoryFilter 
-            selectedCategories={selectedCateg}
-            onChangeCategory={handleCategChange}
-          />
-        </div>
-        <div className="col-span-10">
-          <Products products={filteredProd} />
-        </div>
+    <div className="grid grid-cols-12 my-3">
+
+      <div className="col-span-2 space-y-2">
+
+        <CategoryFilter 
+          selectedCategories={selectedCateg}
+          onChangeCategory={handleCategChange}
+        />
+        <Rating onFilter={handleRatingFilter} />
+
       </div>
+      <div className="col-span-10">
+        <Products products={filteredProd} />
+      </div>
+
     </div>
   );
 }
---
+++ reactjs-collection-page/src/components/Rating.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+function Rating({ onFilter }) {
+
+  const [selectedRat, setSelectedRat] = useState(null);
+  const ratingArr = [5, 4, 3, 2, 1];
+
+  const handleClick = (rat) => {
+    setSelectedRat(rat);
+    onFilter && onFilter(rat);
+  };
+
+  return (
+    <div className="border bg-white border-gray-200 rounded-lg shadow-lg px-5 py-4">
+
+      {ratingArr.map((rat , i) => (
+
+        <div key={i} className="h-8 flex flex-row cursor-pointer items-center space-x-0.5"
+          onClick={() => handleClick(rat)}>
+
+          {Array(5).fill().map((_, i) => (
+            <i key={i}
+              className={`
+                fa-solid fa-star text-[14px] pr-5 
+                ${i < rat ? "text-yellow-400" : "text-gray-300"}
+                ${rat === selectedRat ? "!text-[#e85151]" : ""}
+            `}></i>
+          ))}
+
+          <p className="flex items-end font-medium pl-1 text-gray-500">
+            {rat === 5 ? "5.0" : rat.toFixed(1) + "+"}
+          </p>
+          
+        </div>
+
+      ))}
+      
+    </div>
+  );
+}
+
+export default Rating;
